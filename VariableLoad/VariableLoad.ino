763b13064010ae086a3e6b5da2f3f939401d6302
Warrior87 <buerk1a@cmich.edu>
2020-05-18 14:26:37 -0400

    Update VariableLoad.ino
    removed all charge modes, and most cal mode searches

--- VariableLoad/VariableLoad.ino
+++ VariableLoad/VariableLoad.ino
@@ -1,6 +1,4 @@
 /*TO DO*/
-/*store all values in EEPROM when they change*/
-/*read all values from EEPROM on reset*/
 
 #include <EEPROM.h>
 
@@ -9,14 +7,12 @@ byte cal_Mode_Pin = 12;
 byte LPF_Cal_Pin = 0;
 byte batt1_Pin = 1;
 byte ch1CurrentSetPin = 3;
-byte charge_EN1_Pin = 2;                                      /*active low*/
 
 double batt1Voltage;
 double LPF_Cal_Voltage;
 boolean calModeEN;
 int prev_ch1CurrentSetVal;
 int ch1CurrentSetVal;
-byte charge1EN = 0;
 
 byte ch1CurrentSetValAddress = 0;                             /*EEPROM Addresses*/
 byte charge1ENAddress = 2;
@@ -27,14 +23,12 @@ void setup() {
 
   pinMode(cal_Mode_Pin, INPUT);
   pinMode(ch1CurrentSetPin, OUTPUT);
-  pinMode(charge_EN1_Pin, OUTPUT);
 
   getConfigData();
 }
 
 void loop() {
 
-  calModeEN = digitalRead(cal_Mode_Pin);                                              /*get values and states of relevent pins*/
   batt1Voltage = analogRead(batt1_Pin);
   batt1Voltage = (batt1Voltage * 5.0) / 1023.0;                                       /*convert batt1Voltage from ADC value to Voltage*/
   
@@ -42,23 +36,12 @@ void loop() {
   {
     LPF_Cal_Voltage = analogRead(LPF_Cal_Pin);
   }
-
-  if(!charge1EN)                                                                        /*Discharge Mode*/
+  
+  if(ch1CurrentSetVal != prev_ch1CurrentSetVal)                                       /*if channel 1 current set value is different, write the value*/
   {
-    if(ch1CurrentSetVal != prev_ch1CurrentSetVal)                                       /*if channel 1 current set value is different, write the value*/
-    {
-      analogWrite(ch1CurrentSetPin, ch1CurrentSetVal);
-      prev_ch1CurrentSetVal = ch1CurrentSetVal;
-    }
-  }
-  else
-  {
-    ch1CurrentSetVal = 0;
     analogWrite(ch1CurrentSetPin, ch1CurrentSetVal);
     prev_ch1CurrentSetVal = ch1CurrentSetVal;
   }
-
-  digitalWrite(charge_EN1_Pin, !charge1EN);                                           /*logic is active low for charge enable*/
   
   if(Serial.available() > 0){                                                         /*if there is data from the serial monitor, recieve it*/
     getData();
@@ -88,18 +71,16 @@ void getData(){
     prev_ch1CurrentSetVal = ch1CurrentSetVal;
     ch1CurrentSetVal = dataString.toInt();                                            /*convert that string into an integar*/
     dataString = Serial.readStringUntil(':');
-    charge1EN = dataString.toInt();
+    //charge1EN = dataString.toInt();
 
     ch1CurrentSetVal = (ch1CurrentSetVal / 1000.0 + 0.008505) / (2.0 * 0.017686);     /*ch1CurrentSetVal is in mA, convert to PWM int value*/
-    storeConfigData();
+    storeConfigData();                                                                /*if new setpoints are recieved, store them in EEPROM*/
 }
 
 void storeConfigData(){
     EEPROM.put(ch1CurrentSetValAddress, ch1CurrentSetVal);                            /*store values in EEPROM*/
-    EEPROM.put(charge1ENAddress, charge1EN);
 }
 
 void getConfigData(){
     EEPROM.get(ch1CurrentSetValAddress, ch1CurrentSetVal);
-    EEPROM.get(charge1ENAddress, charge1EN);
 }
